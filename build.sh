87ad93ce13ca1af684500e3f8eb4142328d36eb8
Paul Biggar <paul.biggar@gmail.com>
2019-01-20 20:54:48 -0800

    Add colored output to test suite, add force native tests to run

--- build.sh
+++ build.sh
@@ -2,13 +2,20 @@
 
 set -euo pipefail
 
+echo -e "\n\e[31mBuilding tablecloth-native\e[0m"
+
 pushd native
 dune build
+
+echo -e "\n\e[31mRunning tablecloth-native tests\e[0m"
 cd ../test-native
-dune runtest
+dune runtest -f
 popd
 
+echo -e "\n\e[31mBuilding tablecloth-bucklescript\e[0m"
 pushd bs
 npm run build
+
+echo -e "\n\e[31mRunning tablecloth-bucklescript tests\e[0m"
 npm run test
 popd
