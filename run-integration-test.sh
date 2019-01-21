bfd1d2dfcb6224af276dcaa8af5db739b3aedc8c
Paul Biggar <paul.biggar@gmail.com>
2019-01-21 13:02:17 -0800

    Fix integration tests

--- run-integration-test.sh
+++ run-integration-test.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
+set -x
 
 pushd integration-test/native
-dune build
+echo -e "\n\e[31mBuilding tablecloth-native integration test\e[0m"
+dune build main.exe
+echo -e "\n\e[31mRunning compiled file\e[0m"
+_build/default/main.exe
 popd
 
+echo -e "\n\e[31mBuilding tablecloth-bucklescript integration test\e[0m"
 pushd integration-test/bs
 npm run build
+echo -e "\n\e[31mRunning generated js\e[0m"
+node src/demo.bs.js
 popd
