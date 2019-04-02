e5205dcd2482df947eaa6e7fdb62b96a3c07c81a
Johannes Hoffmann <johannes.m.hoffman@gmx.de>
2019-04-02 19:03:50 +0200

    refactor: make benchmark results clearer

--- bs/src/benchmark_fold_reduce.ml
+++ bs/src/benchmark_fold_reduce.ml
@@ -1,50 +1,47 @@
-open Tablecloth
+let flip f x y = f y x
+
+let foldl_elm ~(f : 'a -> 'b -> 'b) ~(init : 'b) (l : 'a list) : 'b =
+    Belt.List.reduce l init (flip f)
+
+let foldr_elm ~(f : 'a -> 'b -> 'b) ~(init : 'b) (l : 'a list) : 'b =
+    Belt.List.reduceReverse l init (flip f)
+
+let foldl_ocaml ~(f : 'b -> 'a -> 'b) ~(init : 'b) (l : 'a list) : 'b =
+    Belt.List.reduce l init f
+
+let foldr_ocaml ~(f : 'b -> 'a -> 'b) ~(init : 'b) (l : 'a list) : 'b =
+    Belt.List.reduceReverse l init f
 
 let rndInt _ = Js.Math.random_int 0 10
 
 let bench size =
     let str = " list of " ^ (string_of_int size) ^ " random ints" in
-    let randomInts = List.initialize size rndInt in
+    let randomInts = Tablecloth.List.initialize size rndInt in
     Js.Console.log " ";
-    Js.Console.timeStart ("foldl flipped" ^ str);
-    let _ = List.foldl randomInts ~f:(flip (+)) ~init:0 in
-    Js.Console.timeEnd ("foldl flipped" ^ str);
 
-    Js.Console.timeStart ("reduce " ^ str);
-    let _ = Belt.List.reduce randomInts 0 (+) in
-    Js.Console.timeEnd ("reduce " ^ str);
+    Js.Console.timeStart ("foldl ocaml style" ^ str);
+    let _ = foldl_ocaml randomInts ~f:(+) ~init:0 in
+    Js.Console.timeEnd ("foldl ocaml style" ^ str);
 
-    Js.Console.timeStart ("foldr flipped" ^ str);
-    let _ = List.foldr randomInts ~f:(flip (+)) ~init:0 in
-    Js.Console.timeEnd ("foldr flipped" ^ str);
+    Js.Console.timeStart ("foldl elm   style" ^ str);
+    let _ = foldl_elm randomInts ~f:(+) ~init:0 in
+    Js.Console.timeEnd ("foldl elm   style" ^ str);
 
-    Js.Console.timeStart ("reduceReverse " ^ str);
-    let _ = Belt.List.reduceReverse randomInts 0 (+) in
-    Js.Console.timeEnd ("reduceReverse " ^ str);
+    Js.Console.timeStart ("foldr ocaml style" ^ str);
+    let _ = foldr_ocaml randomInts ~f:(+) ~init:0 in
+    Js.Console.timeEnd ("foldr ocaml style" ^ str);
+
+    Js.Console.timeStart ("foldr elm   style" ^ str);
+    let _ = foldr_elm randomInts ~f:(+) ~init:0 in
+    Js.Console.timeEnd ("foldr elm   style" ^ str);
 
     ()
 
 
 let size = 999
 
-let () =
-    bench (size * 1) ;
-    bench (size * 2) ;
-    bench (size * 3) ;
-    bench (size * 4) ;
-    bench (size * 5) ;
-    bench (size * 6) ;
-    bench (size * 7) ;
-    bench (size * 8) ;
-    bench (size * 9) ;
-    bench (size * 10);
-    bench (size * 11);
-    bench (size * 12);
-    bench (size * 13);
-    bench (size * 14);
-    bench (size * 15);
-    bench (size * 16);
-    bench (size * 17);
-    bench (size * 18);
-    bench (size * 19);
-    ()
+let () = begin
+    for i = 1 to 20 do
+    bench (size * i) ;
+    done
+end
