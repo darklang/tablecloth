17b2d53a2d75116bfac55b9fdbc1b2c893192b09
Johannes Hoffmann <johannes.m.hoffman@gmx.de>
2019-04-02 23:05:57 +0200

    style: sort alphabetically

--- bs/src/benchmark_fold_elm_vs_ocaml.ml
+++ bs/src/benchmark_fold_elm_vs_ocaml.ml
@@ -3,11 +3,11 @@ let flip f x y = f y x
 let foldl_elm ~(f : 'a -> 'b -> 'b) ~(init : 'b) (l : 'a list) : 'b =
     Belt.List.reduce l init (flip f)
 
-let foldr_elm ~(f : 'a -> 'b -> 'b) ~(init : 'b) (l : 'a list) : 'b =
-    Belt.List.reduceReverse l init (flip f)
-
 let foldl_ocaml ~(f : 'b -> 'a -> 'b) ~(init : 'b) (l : 'a list) : 'b =
     Belt.List.reduce l init f
+
+let foldr_elm ~(f : 'a -> 'b -> 'b) ~(init : 'b) (l : 'a list) : 'b =
+    Belt.List.reduceReverse l init (flip f)
 
 let foldr_ocaml ~(f : 'b -> 'a -> 'b) ~(init : 'b) (l : 'a list) : 'b =
     Belt.List.reduceReverse l init f
@@ -19,21 +19,21 @@ let bench size =
     let randomInts = Tablecloth.List.initialize size rndInt in
     Js.Console.log " ";
 
-    Js.Console.timeStart ("foldl ocaml style" ^ str);
-    let _ = foldl_ocaml randomInts ~f:(+) ~init:0 in
-    Js.Console.timeEnd ("foldl ocaml style" ^ str);
-
     Js.Console.timeStart ("foldl elm   style" ^ str);
     let _ = foldl_elm randomInts ~f:(+) ~init:0 in
     Js.Console.timeEnd ("foldl elm   style" ^ str);
 
-    Js.Console.timeStart ("foldr ocaml style" ^ str);
-    let _ = foldr_ocaml randomInts ~f:(+) ~init:0 in
-    Js.Console.timeEnd ("foldr ocaml style" ^ str);
+    Js.Console.timeStart ("foldl ocaml style" ^ str);
+    let _ = foldl_ocaml randomInts ~f:(+) ~init:0 in
+    Js.Console.timeEnd ("foldl ocaml style" ^ str);
 
     Js.Console.timeStart ("foldr elm   style" ^ str);
     let _ = foldr_elm randomInts ~f:(+) ~init:0 in
     Js.Console.timeEnd ("foldr elm   style" ^ str);
+
+    Js.Console.timeStart ("foldr ocaml style" ^ str);
+    let _ = foldr_ocaml randomInts ~f:(+) ~init:0 in
+    Js.Console.timeEnd ("foldr ocaml style" ^ str);
 
     ()
 
