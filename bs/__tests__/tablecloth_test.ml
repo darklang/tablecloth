a0a72ba2a35a94e44ee7179ea9f393dbf0009766
Johannes Hoffmann <johannes.m.hoffman@gmx.de>
2019-06-02 17:39:52 +0200

    style: remove whitespace

--- bs/__tests__/tablecloth_test.ml
+++ bs/__tests__/tablecloth_test.ml
@@ -170,158 +170,158 @@ let () =
       expect (Array.flatMap ~f:duplicate [|1; 2; 3|]) |> toEqual [|1; 1; 2; 2; 3; 3|]
     );
 
-    describe "find" (fun () -> 
-      test "returns the first element which `f` returns true for" (fun () -> 
+    describe "find" (fun () ->
+      test "returns the first element which `f` returns true for" (fun () ->
         expect (Array.find ~f:Int.isEven [|1; 3; 4; 8;|]) |> toEqual (Some 4));
 
-      test "returns `None` if `f` returns false for all elements " (fun () -> 
+      test "returns `None` if `f` returns false for all elements " (fun () ->
         expect (Array.find ~f:Int.isOdd [|0; 2; 4; 8;|]) |> toEqual None);
 
-      test "returns `None` for an empty array" (fun () -> 
+      test "returns `None` for an empty array" (fun () ->
         expect (Array.find ~f:Int.isEven [||]) |> toEqual None);
     );
 
-    describe "any" (fun () -> 
-      test "returns false for empty arrays" (fun () -> 
+    describe "any" (fun () ->
+      test "returns false for empty arrays" (fun () ->
         expect (Array.any [||] ~f:Int.isEven) |> toEqual false
       );
 
-      test "returns true if at least one of the elements of an array return true for [f]" (fun () -> 
+      test "returns true if at least one of the elements of an array return true for [f]" (fun () ->
         expect (Array.any [|1;3;4;5;7|] ~f:Int.isEven) |> toEqual true
       );
 
-      test "returns false if all of the elements of an array return false for [f]" (fun () -> 
+      test "returns false if all of the elements of an array return false for [f]" (fun () ->
         expect (Array.any [|1;3;5;7|] ~f:Int.isEven) |> toEqual false
       );
     );
 
-    describe "all" (fun () -> 
-      test "returns true for empty arrays" (fun () -> 
+    describe "all" (fun () ->
+      test "returns true for empty arrays" (fun () ->
         expect (Array.all ~f:Int.isEven [||]) |> toEqual true);
 
-      test "returns true if [f] returns true for all elements" (fun () -> 
+      test "returns true if [f] returns true for all elements" (fun () ->
         expect (Array.all ~f:Int.isEven [|2;4|]) |> toEqual true);
 
       test "returns false if a single element fails returns false for [f]" (fun () ->
         expect (Array.all ~f:Int.isEven [|2;3|]) |> toEqual false);
     );
 
-    test "append" (fun () -> 
+    test "append" (fun () ->
       expect (Array.append (Array.repeat ~length:2 42) (Array.repeat ~length:3 81)) |> toEqual  [|42; 42; 81; 81; 81|]
     );
 
-    test "concatenate" (fun () -> 
+    test "concatenate" (fun () ->
       expect (Array.concatenate [|[|1; 2|]; [|3|]; [|4; 5|]|]) |> toEqual [|1; 2; 3; 4; 5|]);
 
-    describe "intersperse" (fun () -> 
-      test "equals an array literal of the same value" (fun () -> 
-        expect (Array.intersperse ~sep:"on" [|"turtles"; "turtles"; "turtles"|]) 
+    describe "intersperse" (fun () ->
+      test "equals an array literal of the same value" (fun () ->
+        expect (Array.intersperse ~sep:"on" [|"turtles"; "turtles"; "turtles"|])
         |> toEqual [|"turtles"; "on"; "turtles"; "on"; "turtles"|]
       );
 
-      test "equals an array literal of the same value" (fun () -> 
+      test "equals an array literal of the same value" (fun () ->
         expect (Array.intersperse ~sep:0 [||]) |> toEqual [||]);
     );
 
-    describe "slice" (fun () -> 
+    describe "slice" (fun () ->
       let array = [|0; 1; 2; 3; 4|] in
       let positiveArrayLengths = [Array.length array; Array.length array + 1; 1000] in
       let negativeArrayLengths = List.map ~f:Int.negate positiveArrayLengths in
 
-      test "should work with a positive `from`" (fun () -> 
+      test "should work with a positive `from`" (fun () ->
         expect (Array.slice ~from:1 array) |> toEqual [|1; 2; 3; 4|]);
 
-      test "should work with a negative `from`" (fun () -> 
+      test "should work with a negative `from`" (fun () ->
         expect (Array.slice ~from:(-1) array) |> toEqual [|4|]);
 
-      testAll "should work when `from` >= `length`" positiveArrayLengths (fun from -> 
+      testAll "should work when `from` >= `length`" positiveArrayLengths (fun from ->
         expect (Array.slice ~from array) |> toEqual [||]);
 
-      testAll "should work when `from` <= negative `length`" negativeArrayLengths (fun from -> 
+      testAll "should work when `from` <= negative `length`" negativeArrayLengths (fun from ->
         expect (Array.slice ~from array) |> toEqual array);
 
-      test "should work with a positive `to_`" (fun () -> 
+      test "should work with a positive `to_`" (fun () ->
         expect (Array.slice ~from:0  ~to_:3 array) |> toEqual [|0; 1; 2|]);
 
-      test "should work with a negative `to_`" (fun () -> 
+      test "should work with a negative `to_`" (fun () ->
         expect (Array.slice  ~from:1 ~to_:(-1) array) |> toEqual [|1; 2; 3|]);
 
-      testAll "should work when `to_` >= length" positiveArrayLengths (fun to_ -> 
+      testAll "should work when `to_` >= length" positiveArrayLengths (fun to_ ->
         expect (Array.slice ~from:0  ~to_ array) |> toEqual array);
 
-      testAll "should work when `to_` <= negative `length`" negativeArrayLengths (fun to_ -> 
+      testAll "should work when `to_` <= negative `length`" negativeArrayLengths (fun to_ ->
         expect (Array.slice ~from:0  ~to_ array) |> toEqual [||]);
-      
-      test "should work when both `from` and `to_` are negative and `from` < `to_`" (fun () -> 
+
+      test "should work when both `from` and `to_` are negative and `from` < `to_`" (fun () ->
         expect (Array.slice ~from:(-2)  ~to_:(-1) array) |> toEqual [|3|]);
 
-      test "works when `from` >= `to_`" (fun () -> 
+      test "works when `from` >= `to_`" (fun () ->
         expect (Array.slice ~from:(4)  ~to_:(3) array) |> toEqual [||]);
     );
-     
-    describe "foldLeft" (fun () -> 
-      test "works for an empty array" (fun () -> 
+
+    describe "foldLeft" (fun () ->
+      test "works for an empty array" (fun () ->
         expect (Array.foldLeft [||] ~f:(^) ~initial:"") |> toEqual "");
 
-      test "works for an ascociative operator" (fun () -> 
+      test "works for an ascociative operator" (fun () ->
         expect (Array.foldLeft ~f:( * ) ~initial:1 (Array.repeat ~length:4 7)) |> toEqual 2401);
 
-      test "works when the order of arguments to `f` is important" (fun () -> 
+      test "works when the order of arguments to `f` is important" (fun () ->
         expect (Array.foldLeft [|"a";"b";"c"|] ~f:(^) ~initial:"") |> toEqual "cba");
 
-      test "works when the order of arguments to `f` is important" (fun () -> 
+      test "works when the order of arguments to `f` is important" (fun () ->
         expect (Array.foldLeft ~f:(fun element list -> element :: list) ~initial:[] [|1; 2; 3|]) |> toEqual [3; 2; 1]);
     );
 
-    describe "foldRight" (fun () -> 
-      test "works for empty arrays" (fun () -> 
+    describe "foldRight" (fun () ->
+      test "works for empty arrays" (fun () ->
         expect (Array.foldRight [||] ~f:(^) ~initial:"") |> toEqual "");
 
-      test "fold right" (fun () -> 
+      test "fold right" (fun () ->
         expect (Array.foldRight ~f:(+) ~initial:0 (Array.repeat ~length:3 5)) |> toEqual 15);
 
-      test "works when the order of arguments to `f` is important" (fun () -> 
+      test "works when the order of arguments to `f` is important" (fun () ->
         expect (Array.foldRight [|"a";"b";"c"|] ~f:(^) ~initial:"") |> toEqual "abc");
-        
-      test "works when the order of arguments to `f` is important" (fun () -> 
+
+      test "works when the order of arguments to `f` is important" (fun () ->
         expect (Array.foldRight ~f:(fun element list -> element :: list) ~initial:[] [|1; 2; 3|]) |> toEqual [1; 2; 3]);
     );
 
-    describe "reverse" (fun () -> 
+    describe "reverse" (fun () ->
       test "reverse empty array" (fun () -> expect (Array.reverse [||]) |> toEqual [||]);
       test "reverse two elements" (fun () -> expect (Array.reverse [|0;1|]) |> toEqual [|1;0|]);
-      test "leaves the original array untouched" (fun () -> 
+      test "leaves the original array untouched" (fun () ->
         let array = [|0; 1; 2; 3;|] in
         let _reversedArray = Array.reverse array in
         expect array |> toEqual [|0; 1; 2; 3;|]
       );
     );
 
-    describe "reverseInPlace" (fun () -> 
-      test "alters an array in-place" (fun () -> 
+    describe "reverseInPlace" (fun () ->
+      test "alters an array in-place" (fun () ->
         let array = [|1;2;3|] in
         Array.reverseInPlace array;
         expect array |> toEqual [|3;2;1|]
       );
     );
 
-    test "forEach" (fun () -> 
+    test "forEach" (fun () ->
       let index = ref 0 in
       let calledValues = [|0;0;0|] in
-      
-      Array.forEach [|1;2;3|] ~f:(fun value -> 
+
+      Array.forEach [|1;2;3|] ~f:(fun value ->
         Array.set calledValues ~index:!index ~value;
         index := !index + 1;
       );
-      
+
       expect calledValues |> toEqual [|1;2;3|]
     );
   );
 
-  describe "Char" (fun () -> 
+  describe "Char" (fun () ->
     test "toCode" (fun () -> expect (Char.toCode 'a') |> toEqual 97);
 
-    describe "fromCode" (fun () -> 
+    describe "fromCode" (fun () ->
       test "valid ASCII codes return the corresponding character" (fun () -> expect (Char.fromCode 97) |> toEqual (Some 'a'));
       test "negative integers return none" (fun () -> expect (Char.fromCode (-1)) |> toEqual None);
       test "integers greater than 255 return none" (fun () -> expect (Char.fromCode 256) |> toEqual None);
@@ -329,69 +329,69 @@ let () =
 
     test "toString" (fun () -> expect (Char.toString 'a') |> toEqual "a");
 
-    describe "fromString" (fun () -> 
+    describe "fromString" (fun () ->
       test "one-length string return Some" (fun () -> expect (Char.fromString "a") |> toEqual (Some 'a'));
       test "multi character strings return none" (fun () -> expect (Char.fromString "abc") |> toEqual None);
       test "zero length strings return none" (fun () -> expect (Char.fromString "") |> toEqual None);
     );
 
-    describe "toLowercase" (fun () -> 
+    describe "toLowercase" (fun () ->
       test "converts uppercase ASCII characters to lowercase" (fun () -> expect (Char.toLowercase 'A') |> toEqual 'a');
       test "perserves lowercase characters" (fun () -> expect (Char.toLowercase 'a') |> toEqual 'a');
       test "perserves non-alphabet characters" (fun () -> expect (Char.toLowercase '7') |> toEqual '7');
       test "perserves non-ASCII characters" (fun () -> expect (Char.toUppercase '\233') |> toEqual '\233');
     );
 
-    describe "toUppercase" (fun () -> 
+    describe "toUppercase" (fun () ->
       test "converts lowercase ASCII characters to uppercase" (fun () -> expect (Char.toUppercase 'a') |> toEqual 'A');
       test "perserves uppercase characters" (fun () -> expect (Char.toUppercase 'A') |> toEqual 'A');
       test "perserves non-alphabet characters" (fun () -> expect (Char.toUppercase '7') |> toEqual '7');
       test "perserves non-ASCII characters" (fun () -> expect (Char.toUppercase '\233') |> toEqual '\233');
     );
 
-    describe "toDigit" (fun () -> 
+    describe "toDigit" (fun () ->
       test "toDigit - converts ASCII characters representing digits into integers" (fun () -> expect (Char.toDigit '0') |> toEqual (Some 0));
       test "toDigit - converts ASCII characters representing digits into integers" (fun () -> expect (Char.toDigit '8') |> toEqual (Some 8));
       test "toDigit - converts ASCII characters representing digits into integers" (fun () -> expect (Char.toDigit 'a') |> toEqual None);
     );
 
 
-    describe "isLowercase" (fun () -> 
+    describe "isLowercase" (fun () ->
       test "returns true for any lowercase character" (fun () -> expect (Char.isLowercase 'a') |> toEqual true);
-      test "returns false for all other characters" (fun () -> expect (Char.isLowercase '7') |> toEqual false);      
-      test "returns false for non-ASCII characters" (fun () -> expect (Char.isLowercase '\236') |> toEqual false);      
+      test "returns false for all other characters" (fun () -> expect (Char.isLowercase '7') |> toEqual false);
+      test "returns false for non-ASCII characters" (fun () -> expect (Char.isLowercase '\236') |> toEqual false);
     );
 
-    describe "isUppercase" (fun () -> 
+    describe "isUppercase" (fun () ->
       test "returns true for any uppercase character" (fun () -> expect (Char.isUppercase 'A') |> toEqual true);
-      test "returns false for all other characters" (fun () -> expect (Char.isUppercase '7') |> toEqual false);      
-      test "returns false for non-ASCII characters" (fun () -> expect (Char.isLowercase '\237') |> toEqual false);      
+      test "returns false for all other characters" (fun () -> expect (Char.isUppercase '7') |> toEqual false);
+      test "returns false for non-ASCII characters" (fun () -> expect (Char.isLowercase '\237') |> toEqual false);
     );
 
-    describe "isLetter" (fun () -> 
+    describe "isLetter" (fun () ->
       test "returns true for any ASCII alphabet character" (fun () -> expect (Char.isLetter 'A') |> toEqual true);
-      testAll "returns false for all other characters" ['7'; ' '; '\n'; '\011'; '\236'] (fun char -> expect (Char.isLetter char) |> toEqual false);      
+      testAll "returns false for all other characters" ['7'; ' '; '\n'; '\011'; '\236'] (fun char -> expect (Char.isLetter char) |> toEqual false);
     );
 
-    describe "isDigit" (fun () -> 
+    describe "isDigit" (fun () ->
       testAll "returns true for digits 0-9" ['0'; '1'; '2'; '3'; '4'; '5'; '6'; '7'; '8'; '9';] (fun digit -> expect (Char.isDigit digit) |> toEqual true);
       test "returns false for all other characters" (fun () -> expect (Char.isDigit 'a') |> toEqual false);
     );
 
-    describe "isAlphanumeric" (fun () -> 
+    describe "isAlphanumeric" (fun () ->
       test "returns true for any alphabet or digit character" (fun () -> expect (Char.isAlphanumeric 'A') |> toEqual true);
-      test "returns false for all other characters" (fun () -> expect (Char.isAlphanumeric '?') |> toEqual false);      
+      test "returns false for all other characters" (fun () -> expect (Char.isAlphanumeric '?') |> toEqual false);
     );
- 
-    describe "isPrintable" (fun () -> 
+
+    describe "isPrintable" (fun () ->
       test "returns true for a printable character" (fun () -> expect (Char.isPrintable '~') |> toEqual true);
 
-      test "returns false for non-printable character" (fun () -> expect (Char.fromCode 31 |> Option.map ~f:Char.isPrintable ) |> toEqual (Some false));      
-    );   
+      test "returns false for non-printable character" (fun () -> expect (Char.fromCode 31 |> Option.map ~f:Char.isPrintable ) |> toEqual (Some false));
+    );
 
-    describe "isWhitespace" (fun () -> 
+    describe "isWhitespace" (fun () ->
       test "returns true for any whitespace character" (fun () -> expect (Char.isWhitespace ' ') |> toEqual true);
-      test "returns false for a non-whitespace character" (fun () -> expect (Char.isWhitespace 'a') |> toEqual false);      
+      test "returns false for a non-whitespace character" (fun () -> expect (Char.isWhitespace 'a') |> toEqual false);
     );
   );
 
@@ -419,27 +419,27 @@ let () =
       test "one element" (fun () -> expect (List.partition ~f:(fun x -> x mod 2 = 0) [1]) |> toEqual ([], [1]));
       test "four elements" (fun () -> expect (List.partition ~f:(fun x -> x mod 2 = 0) [1;2;3;4]) |> toEqual ([2;4], [1;3]));
     );
- 
+
     describe "minimumBy" (fun () ->
       test "minimumBy non-empty list" (fun () -> expect (List.minimumBy ~f:(fun x -> x mod 12) [7;9;15;10;3;22]) |> toEqual (Some 15));
       test "minimumBy empty list" (fun () -> expect (List.minimumBy ~f:(fun x -> x mod 12) []) |> toEqual None);
     );
-    
+
     describe "maximumBy" (fun () ->
       test "maximumBy non-empty list" (fun () -> expect (List.maximumBy ~f:(fun x -> x mod 12) [7;9;15;10;3;22]) |> toEqual (Some 10));
       test "maximumBy empty list" (fun () -> expect (List.maximumBy ~f:(fun x -> x mod 12) []) |> toEqual None);
     );
-    
+
     describe "minimum" (fun () ->
       test "minimum non-empty list" (fun () -> expect (List.minimum [7; 9; 15; 10; 3]) |> toEqual (Some 3));
       test "minimum empty list" (fun () -> expect (List.minimum []) |> toEqual None);
     );
-    
+
     describe "maximum" (fun () ->
       test "maximum non-empty list" (fun () -> expect (List.maximum [7; 9; 15; 10; 3]) |> toEqual (Some 15));
       test "maximum empty list" (fun () -> expect (List.maximum []) |> toEqual None);
     );
-   
+
     describe "split_when" (fun () ->
       test "empty list" (fun () -> expect (List.split_when ~f:(fun x -> x mod 2 = 0) []) |> toEqual ([], []));
       test "at zero" (fun () -> expect (List.split_when ~f:(fun x -> x mod 2 = 0) [2;4;6]) |> toEqual ([], [2;4;6]));
