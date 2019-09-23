f83b892031cd1af8376f08cd88198dbd3829d68b
Martin Svanberg <martin@martinsvanberg.com>
2019-09-23 19:12:23 +0200

    Add separate source file for OCaml version compatibility

--- bs/src/tablecloth.ml
+++ bs/src/tablecloth.ml
@@ -1205,32 +1205,7 @@ module String = struct
 
   let starts_with = startsWith
 
-  (* Silence warnings in BS 6: https://github.com/darklang/tablecloth/pull/88 *)
-#if OCAML_VERSION =~ ">=4.03" then
-
-  let toLower (s : string) : string = String.lowercase_ascii s
-
-  let toUpper (s : string) : string = String.uppercase_ascii s
-
-  let uncapitalize (s : string) : string = String.uncapitalize_ascii s
-
-  let capitalize (s : string) : string = String.capitalize_ascii s
-
-  let isCapitalized (s : string) : bool = s = String.capitalize_ascii s
-
-#else
-
-  let toLower (s : string) : string = String.lowercase s
-
-  let toUpper (s : string) : string = String.uppercase s
-
-  let uncapitalize (s : string) : string = String.uncapitalize s
-
-  let capitalize (s : string) : string = String.capitalize s
-
-  let isCapitalized (s : string) : bool = s = String.capitalize s
-
-#end
+  include Compat.String
 
   let to_lower = toLower
 
