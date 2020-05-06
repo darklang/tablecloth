2c4b7399315475dc4a5c63cb034a903c1354f2f2
Joe <joe@joefiorini.com>
2020-05-06 15:45:30 -0400

    Make Result.map consistent with other maps

--- bs/__tests__/tablecloth_test.ml
+++ bs/__tests__/tablecloth_test.ml
@@ -10,7 +10,12 @@ let () =
               |> toEqual (Belt.Result.Error "error message")) ;
           test "maps Some into Ok" (fun () ->
               expect Result.(fromOption ~error:"error message" (Some 10))
-              |> toEqual (Belt.Result.Ok 10)))) ;
+              |> toEqual (Belt.Result.Ok 10))) ;
+      describe "map" (fun () ->
+          test "maps value on success" (fun () ->
+              expect Result.(map ~f:String.reverse (succeed "blah"))
+              |> toEqual (Belt.Result.Ok "halb")))) ;
+
   describe "Fun" (fun () ->
       test "identity" (fun () -> expect (Fun.identity 1) |> toEqual 1) ;
       test "ignore" (fun () -> expect (Fun.ignore 1) |> toEqual ()) ;
