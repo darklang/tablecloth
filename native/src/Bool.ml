8e0f35c31012dda7f1c4cc23cfd96e4ee8284ea9
Dean Merchant <deanmerchant@gmail.com>
2020-05-20 18:54:19 +0100

    Share the Bool interface file between implementations

---
+++ native/src/Bool.ml
@@ -0,0 +1,33 @@
+type t = bool
+
+let fromInt i = match i with 0 -> Some false | 1 -> Some true | _ -> None
+
+let from_int = fromInt
+
+let fromString string =
+  match string with "false" -> Some false | "true" -> Some true | _ -> None
+
+
+let from_string = fromString
+
+external ( && ) : bool -> bool -> bool = "%sequand"
+
+external ( || ) : bool -> bool -> bool = "%sequor"
+
+let xor a b = (a && not b) || ((not a) && b)
+
+let not = not
+
+let negate f t = not (f t)
+
+let equal = ( = )
+
+let compare = compare
+
+let toString = function true -> "true" | false -> "false"
+
+let to_string = toString
+
+let toInt t = match t with true -> 1 | false -> 0
+
+let to_int = toInt
--- native/src/tablecloth.ml
+++ native/src/tablecloth.ml
@@ -1,34 +1,4 @@
-module Bool = struct
-  type t = bool
-
-  let fromInt i = match i with 0 -> Some false | 1 -> Some true | _ -> None
-
-  let from_int = fromInt
-
-  let fromString string =
-    match string with "false" -> Some false | "true" -> Some true | _ -> None
-
-
-  let from_string = fromString
-
-  external ( && ) : bool -> bool -> bool = "%sequand"
-
-  external ( || ) : bool -> bool -> bool = "%sequor"
-
-  let xor a b = (a && not b) || ((not a) && b)
-
-  let not = not
-
-  let negate f t = not (f t)
-
-  let equal = ( = )
-
-  let compare = compare
-
-  let toString = function true -> "true" | false -> "false"
-
-  let toInt t = match t with true -> 1 | false -> 0
-end
+module Bool = Bool
 
 module Fun = struct
   external identity : 'a -> 'a = "%identity"
