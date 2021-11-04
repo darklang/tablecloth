b3d61ee335801dac4cb7cc84855a4165ac54f5ea
Work Kate <hello.kategrant@gmail.com>
2021-11-04 11:08:17 -0400

    added test description for Result.ok

--- rescript/test/ResultTest.ml
+++ rescript/test/ResultTest.ml
@@ -4,6 +4,19 @@ open AlcoJest
 let suite =
   suite "Result" (fun () ->
       let open Result in
+      describe "ok" (fun () ->
+          test "returns ok type" (fun () ->
+              expect (String.reverse "desserts" |> Result.ok)
+              |> toEqual
+                   (let open Eq in
+                   result string string)
+                   (Ok "stressed") ) ;
+          test "returns ok type" (fun () ->
+              expect (List.map [ 1; 2; 3 ] ~f:Result.ok)
+              |> toEqual
+                   (let open Eq in
+                    list (result int string))
+                   [ Ok 1; Ok 2; Ok 3 ] ) ) ;
       describe "fromOption" (fun () ->
           test "maps None into Error" (fun () ->
               expect (fromOption ~error:"error message" None)
